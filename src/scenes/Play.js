56a911a288c1fbf839a6592cff9f9af067aefe94
danielledk <dkraljev@ucsc.edu>
2021-04-11 13:18:13 -0700

    starfiel asset implementation

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -20,6 +20,8 @@ class Play extends Phaser.Scene {
             borderUISize*2,
             0x00FF00,
             ).setOrigin(0,0);
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
