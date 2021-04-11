ce59104f94bea308b81d23a90b3067f841df1f41
danielledk <dkraljev@ucsc.edu>
2021-04-11 12:46:37 -0700

    Adding Green UI

---
+++ src/scenes/Play.js
@@ -0,0 +1,17 @@
+
+
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+    
+    create() {
+        //green UI
+        this.add.rectangle(0, 
+            borderUISize + borderPadding, 
+            game.config.width, 
+            borderUISize*2,
+            0x00FF00,
+            ).setOrigin(0,0);
+    }
+}
