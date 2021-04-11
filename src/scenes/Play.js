514cf17e70daba93b1a2d1ada06876e25278fb0c
danielledk <dkraljev@ucsc.edu>
2021-04-11 13:12:56 -0700

    Asset preloads

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,7 +4,14 @@ class Play extends Phaser.Scene {
     constructor() {
         super("playScene");
     }
-    
+
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+      }
+
     create() {
         //green UI
         this.add.rectangle(0, 
@@ -13,5 +20,10 @@ class Play extends Phaser.Scene {
             borderUISize*2,
             0x00FF00,
             ).setOrigin(0,0);
+        // white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
 }
